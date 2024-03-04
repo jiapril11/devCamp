739fe0e68006b3ba94ac4797f61926720821542b
jiapril11 <tadadadacoding@gmail.com>
2024-03-04 20:11:39 +0900

    feat: react hook form 적용

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,50 +1,57 @@
 "use client";
 
-import { useState } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
-import { z } from "zod";
 
-const userNameSchema = z.string().min(3).max(25);
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { DevTool } from "@hookform/devtools";
+
+const userSchema = z.object({
+  userName: z.string().min(3).max(25),
+});
 
 export default function SignUp() {
-  const [userName, setUserName] = useState("");
-  const [error, setError] = useState("");
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(userSchema),
+    mode: "onChange",
+  });
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    try {
-      userNameSchema.parse(userName);
-      setError("");
-    } catch (err) {
-      if (err instanceof z.ZodError) {
-        setError(`🚫 ${err.errors[0].message}`);
-      }
-    }
-  };
   return (
-    <section className="flex flex-col justify-center items-center min-h-screen">
-      <div className="w-96 border p-3 box-border">
-        <h1 className="font-bold">Sign Up</h1>
-        <form onSubmit={handleSubmit}>
-          <div className="my-3">
-            <label htmlFor="userName" className="mb-1">
-              name
-            </label>
-            <Input
-              id="userName"
-              type="text"
-              placeholder="type your name."
-              value={userName}
-              onChange={(e) => setUserName(e.target.value)}
-            />
-            <p className={`mt-1 pl-2 min-h-5 text-sm font-light text-red-500`}>
-              {error ? error : ""}
-            </p>
-          </div>
-          <Button type="submit">Submit</Button>
-        </form>
-      </div>
-    </section>
+    <>
+      <section className="flex flex-col justify-center items-center min-h-screen">
+        <div className="w-96 border p-3 box-border">
+          <h1 className="font-bold">Sign Up</h1>
+          <form onSubmit={handleSubmit((data) => console.log(data))}>
+            <div className="my-3">
+              <label htmlFor="userName" className="mb-1">
+                name
+              </label>
+              <Input
+                id="userName"
+                type="text"
+                placeholder="type your name."
+                {...register("userName", { required: true, maxLength: 20 })}
+              />
+              <p
+                className={`mt-1 pl-2 min-h-5 text-sm font-light text-red-500`}
+              >
+                {errors.userName?.message
+                  ? errors.userName?.message.toString()
+                  : ""}
+              </p>
+            </div>
+            <Button type="submit">Submit</Button>
+          </form>
+        </div>
+      </section>
+      <DevTool control={control} />
+    </>
   );
 }
